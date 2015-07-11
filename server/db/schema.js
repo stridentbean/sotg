1cce6bc149bf7fe304fce7ab4ba23ecf6a55c661
Medhir Bhargava <medhirbhargava@gmail.com>
2015-07-10 17:36:53 -0700

    Add tables for tweets

--- server/db/schema.js
+++ server/db/schema.js
@@ -42,6 +42,29 @@ db.knex.schema.hasTable('User').then(function(exists) {
   }
 });
 
+db.knex.schema.hasTable('Tweet').then(function(exists) {
+  if(!exists) {
+    db.knex.schema.createTable('Tweet', function(tweet) {
+      tweet.bigInteger('id');
+      tweet.string('text');
+      tweet.string('GPS');
+      tweet.timestamp('timestamp');
+      //hashtags as a foreign key
+      tweet.json('urls');
+
+    });
+  }
+});
+
+db.knex.schema.hasTable('Hashtag').then(function(exists) {
+  if(!exists) {
+    db.knex.schema.createTable('Hashtag', function(hashtag) {
+      hashtag.increments('id').primary();
+      hashtag.string('tag');
+    });
+  }
+});
+
 /**
   * Remove all tables from the database 
   *@arg next {function} Function to run after truncation is complete
