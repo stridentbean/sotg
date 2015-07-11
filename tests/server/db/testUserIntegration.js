85a0ee4d7455e0b577c00d484b1334718de3ad57
Michael Borglin <Borglin.Me@gmail.com>
2015-07-11 15:52:12 -0700

    BugFix tests should now get env port number on circleci

--- tests/server/db/testUserIntegration.js
+++ tests/server/db/testUserIntegration.js
@@ -2,7 +2,8 @@ var User = require('../../../server/users/userModel.js'),
   db = require('../../../server/db/schema.js'),
   request = require('request'),
   Q = require('q'),
-  should = require('chai').should();
+  should = require('chai').should(),
+  PORT = process.env.MYSQL_DATABASE_USER || 8000;
 
 /**
  * Describes how a user account is created
@@ -46,7 +47,7 @@ describe('Account Creation', function() {
   it('Signup should reject a bad username', function(next) {
     var options = {
       'method': 'POST',
-      'uri': 'http://localhost:8000/users/signup',
+      'uri': 'http://localhost:' + PORT + '/users/signup',
       'json': {
         'username': 'userNotAnEmail',
         'password': PASS,
@@ -73,7 +74,7 @@ describe('Account Signin', function() {
     db.truncateAllTables(function() {
       var options = {
         'method': 'POST',
-        'uri': 'http://127.0.0.1:8000/users/signup',
+        'uri': 'http://127.0.0.1:' + PORT + '/users/signup',
         'json': {
           'username': USER,
           'password': PASS,
@@ -89,7 +90,7 @@ describe('Account Signin', function() {
   it('Signin to a user record', function(next) {
     var options = {
       'method': 'POST',
-      'uri': 'http://127.0.0.1:8000/users/signin',
+      'uri': 'http://127.0.0.1:' + PORT + '/users/signin',
       'json': {
         'username': USER,
         'password': PASS,
@@ -106,7 +107,7 @@ describe('Account Signin', function() {
   it('Signin should reject a bad username/password combo', function(next) {
     var options = {
       'method': 'POST',
-      'uri': 'http://127.0.0.1:8000/users/signin',
+      'uri': 'http://127.0.0.1:' + PORT + '/users/signin',
       'json': {
         'username': 'userNotAnEmail',
         'password': PASS,
