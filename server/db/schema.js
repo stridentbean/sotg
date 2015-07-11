995cee7682175f052565bd3cc40a59d365039c74
Eric Ihli <ericihli@gmail.com>
2015-07-11 12:19:07 -0700

    (refactor) Remove env variables from config and add to schema

--- server/db/schema.js
+++ server/db/schema.js
@@ -8,10 +8,10 @@ var knex = require('knex')({
   client: 'mysql',
   //TODO find the proper address for the 
   connection: process.env.CLEARDB_DATABASE_URL || {
-    host: config.host,
-    user: config.user,
-    password: config.password,
-    database: config.database,
+    host: '127.0.0.1' || config.host, // TODO: Set hardcoded host as env variable.
+    user: process.env.MYSQL_DATABASE_USER || config.user,
+    password: process.env.MYSQL_DATABASE_PASSWORD || config.password,
+    database: process.env.MYSQL_DATABASE || config.database,
     charset: 'utf8'
   }
 });
