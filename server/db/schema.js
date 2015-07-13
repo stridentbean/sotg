57699af60f6db514717acb68d2bcfc3638b8b16d
Eric Ihli <ericihli@gmail.com>
2015-07-12 21:29:37 -0700

    (refactor) Modify schema and work on Dockerfiles
    Make schema work with Dockerfile environment variable.
    Start working on Dockerfiles.

--- server/db/schema.js
+++ server/db/schema.js
@@ -8,7 +8,7 @@ var knex = require('knex')({
   client: 'mysql',
   //TODO find the proper address for the 
   connection: process.env.CLEARDB_DATABASE_URL || {
-    host: '127.0.0.1' || config.host, // TODO: Set hardcoded host as env variable.
+    host: process.env.MYSQL_DATABASE_SERVER || config.host, // TODO: Set hardcoded host as env variable.
     user: process.env.MYSQL_DATABASE_USER || config.user,
     password: process.env.MYSQL_DATABASE_PASSWORD || config.password,
     database: process.env.MYSQL_DATABASE || config.database,
