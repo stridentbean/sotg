a61d2a9c122fc11bc4e29c49d920f33efcbca070
Eric Ihli <ericihli@gmail.com>
2015-07-14 15:35:37 -0700

    (deployment) Add env variables for rds

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,10 @@ var knex = require('knex')({
   client: 'mysql',
   //TODO find the proper address for the 
   connection: process.env.CLEARDB_DATABASE_URL || {
-    host: process.env.MYSQL_DATABASE_SERVER || config.host, // TODO: Set hardcoded host as env variable.
-    user: process.env.MYSQL_DATABASE_USER || config.user,
-    password: process.env.MYSQL_DATABASE_PASSWORD || config.password,
-    database: process.env.MYSQL_DATABASE || config.database,
+    host: process.env.RDS_HOSTNAME || process.env.MYSQL_DATABASE_SERVER || config.host, // TODO: Set hardcoded host as env variable.
+    user: process.env.RDS_USERNAME || process.env.MYSQL_DATABASE_USER || config.user,
+    password: process.env.RDS_PASSWORD || process.env.MYSQL_DATABASE_PASSWORD || config.password,
+    database: process.env.RDS_DB_NAME || process.env.MYSQL_DATABASE || config.database,
     charset: 'utf8'
   }
 });
