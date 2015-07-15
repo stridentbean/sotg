1ac39fd8d9f74f041837b28b84091f9987efbcdd
Medhir Bhargava <medhirbhargava@gmail.com>
2015-07-15 15:29:02 -0700

    Update tests for new async queue

--- tests/tweetHandler/unit/queue.js
+++ tests/tweetHandler/unit/queue.js
@@ -1,33 +1,37 @@
 var Tweet = require('../../../tweetHandler/tweets/tweetModel.js'),
   db = require('../../../server/db/schema.js'),
-  queue = require('../../../tweetHandler/config/queue.js'),
+  queues = require('../../../tweetHandler/config/queue.js'),
   should = require('chai').should();
 
 /**
- * Describes how the tweet handler should work
+ * Describes async queues
  * @class
  */
 
-describe('Queue', function() {
+describe('Queues', function() {
 
-  before(function(next) {
-    db.truncateAllTables(function(){
-      next();
-    });
+  // before(function(next) {
+  //   db.truncateAllTables(function(){
+  //     next();
+  //   });
 
-  });
+  // });
 
-  describe('Insertion Queue', function() {
+  // describe('Insertion Queue', function() {
 
-    beforeEach(function(next) {
-      var insertionQ = new queue();
-      next();
-    });
+  //   beforeEach(function(next) {
+  //     var insertionQ = new queue();
+  //     next();
+  //   });
 
-    it('should insert a tweet into the DB', function() {
+  //   it('should insert a tweet into the DB', function() {
 
-    });
+  //   });
 
+  // });
+
+  it('should have an async queue for insertion of tweets', function() {
+    queues.insertionQ.should.be.an('object');
   });
 
 });
