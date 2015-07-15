6ece8481f58a122d8a94a471ca3d7c965d2262ee
Sarah Yu <sarah.m.yu88@gmail.com>
2015-07-15 16:43:05 -0700

    (feat) add test for '/addKeyword' endpoint

--- tests/server/integration/apiGeneral.js
+++ tests/server/integration/apiGeneral.js
@@ -14,19 +14,22 @@ describe('API Endpoint Behavior', function() {
   after(function(done) {
     done();
   });
-
-  it('should return keywords on /getKeywords endpoint', function(next){
+  it('should return "success" on /addKeyword endpoint', function(next) {
     var addKeywordOptions = {
-      'uri': 'http://localhost:' + PORT + '/api/addKeyword/?api_key=xxxx&keyword=coke'
+      'uri': 'http://localhost:' + PORT + '/api/addKeyword/?api_key=xxxx&keyword=pizza'
     };
+    request(addKeywordOptions, function(error, res, body) {
+      body.should.deep.equal('success');
+      next();
+    });
+  });
+  it('should return keywords on /getKeywords endpoint', function(next) {
     var getKeywordsOptions = {
       'uri': 'http://localhost:' + PORT + '/api/getKeywords/?api_key=xxxx&number=1'
     };
-    request(addKeywordOptions, function(error, res, body) {
       request(getKeywordsOptions, function(error, res, body) {
-        body.should.deep.equal('["coke"]');
+        body.should.deep.equal('["pizza"]');
         next();
       });
-    });
   });
 });
