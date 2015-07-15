67909dd2f8df2ba0720fe091f1217b8f07dc8d21
Sarah Yu <sarah.m.yu88@gmail.com>
2015-07-14 19:07:56 -0700

    (feat) api routes have console.logs for testing

--- server/api/apiRoutes.js
+++ server/api/apiRoutes.js
@@ -1,7 +1,7 @@
 var apiController = require('./apiController.js');
 
 module.exports = function(app) {
-  app.post('/key', apiController.getKey);
+  app.get('/key', apiController.getKey);
   app.get('/search', apiController.getSearch);
   app.get('/location', apiController.getLocation);
   app.get('/sentiment', apiController.getSentiment);
