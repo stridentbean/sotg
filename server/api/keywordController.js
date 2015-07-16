96afa5e0ba610c5509811dd7b8203282a97576cd
Eric Ihli <ericihli@gmail.com>
2015-07-16 15:48:27 -0700

    (feat) Finish controller for getKeywords.

--- server/api/keywordController.js
+++ server/api/keywordController.js
@@ -6,33 +6,28 @@ var getLeastUsedStream = function() {
 };
 
 module.exports = {
+  
   addKeyword: function(req, res, next) {
-    // get keyword from request body
     var keyword = req.body.keyword,
-    // get the least used stream
       leastUsedStream = getLeastUsedStream();
     console.log(keyword);
-    // create model
     new Keyword({
       keyword: keyword
     })
     .fetch()
     .then(function(exists) {
       if (exists) {
-        // Updating keyword already in database (not necessary atm)
         return next({
           result: 'update',
           value: exists.get('keyword')
         });
       } else {
-        // Inserting into database
         new Keyword({
           keyword: keyword,
           streamId: leastUsedStream
         })
         .save()
         .then(function(keyword) {
-          // console.log("Inserted ", keyword.get('keyword'));
           return next({
             result: 'insert',
             value: keyword.get('keyword')
@@ -40,9 +35,17 @@ module.exports = {
         });
       }
     });
-    // return success response code 201?
   },
-  getKeywords: function() {
 
+  getKeywords: function(req, res, next) {
+    var streamId = req.body.streamId;
+    var resultArray;
+    new Keyword({streamId: streamId}).fetchAll()
+    .then(function(results) {
+      resultArray = results.map(function(el) {
+        return el.get('keyword');
+      });
+      res.json(resultArray)
+    });
   }
 };
--- streaming/server.js
+++ streaming/server.js
@@ -8,19 +8,21 @@ var request = require('request'),
   HANDLER_PORT = process.env.HANDLER_PORT || 6000; //All handler 
 var credentials;
 
-// try {
-//   stats = fs.lstatSync('./config.js');
-//   if (stats.isDirectory()) {
-//     credentials = require('./config');
-//   } else {
-//     credentials = {
-//       consumer_key: process.env.CONSUMER_KEY, 
-//       consumer_secret: process.env.CONSUMER_SECRET, 
-//       access_token: process.env.ACCESS_TOKEN, 
-//       access_token_secret: process.env.TOKEN_SECRET
-//     };
-//   }
-// }
+try {
+  stats = fs.lstatSync('./config.js');
+  if (stats.isDirectory()) {
+    credentials = require('./config');
+  } else {
+    credentials = {
+      consumer_key: process.env.CONSUMER_KEY, 
+      consumer_secret: process.env.CONSUMER_SECRET, 
+      access_token: process.env.ACCESS_TOKEN, 
+      access_token_secret: process.env.TOKEN_SECRET
+    };
+  }
+}
+catch(err) {
+}
 var T = new Twit(credentials);
 
 //spin up server
