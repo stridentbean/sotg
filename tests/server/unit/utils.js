00b64a20162998ce6b40d4981fd378a6d33bd464
Michael Borglin <Borglin.Me@gmail.com>
2015-07-16 19:42:01 -0700

    mend

--- tests/server/unit/utils.js
+++ tests/server/unit/utils.js
@@ -1,1 +1,22 @@
+var Utils = require('../../../server/config/utils.js'),
+  db = require('../../../server/config/db.js'),
+  should = require('chai').should();
 
+/**
+ * Describes how Utils should work
+ * @class
+ */
+
+describe('Utils', function() {
+
+  it('should validate correct emails', function() {
+    Utils.validateEmail('blarg@gmail.com').should.equal(true);
+  });
+
+  it('should invalidate correct emails', function() {
+    Utils.validateEmail('blarggmailcom').should.equal(false);
+    Utils.validateEmail('blarg@gmailcom').should.equal(false);
+    Utils.validateEmail('blarggmail.com').should.equal(false);
+    Utils.validateEmail('blarg;gmail.com').should.equal(false);
+  });
+});
