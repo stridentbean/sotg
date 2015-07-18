6f2771922bc1f9899ee73b13182e7b4abe076381
Medhir Bhargava <medhirbhargava@gmail.com>
2015-07-18 14:43:41 -0700

    Create API authentication middleware

---
+++ server/api/middleware.js
@@ -0,0 +1,21 @@
+var User = require('../users/userModel.js');
+
+var authAPIKey = module.exports.authAPIKey = function(req, res, next) {
+  var apiKey = req.query.apiKey;
+
+  if(!apiKey) {
+    res.status(404).send('Must provide an API key for this endpoint.');
+  }
+  
+  new User({
+    apiKey: apiKey
+  })
+  .fetch()
+  .then(function(user) {
+    if(user) {
+      next(); //go to next function to resolve API request
+    } else {
+      res.status(404).send('Invalid API key!');
+    }
+  });
+};
