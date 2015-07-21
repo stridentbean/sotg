0d2cbe4566628dceb9a199e01222e4979b77d4fe
Sarah Yu <sarah.m.yu88@gmail.com>
2015-07-21 11:34:46 -0700

    (feat) improved profile error handling

--- client/app/controllers/profileController.js
+++ client/app/controllers/profileController.js
@@ -3,11 +3,11 @@ angular.module('profileCtrl', [])
   var vm = this;
   Auth.profile()
   .then(function(res){
-      vm.username = res.data.username, 
-      vm.apiKey = res.data.apiKey, 
-      vm.keywords = res.data.keywords
+      vm.username = res.data.username; 
+      vm.apiKey = res.data.apiKey;
+      vm.keywords = res.data.keywords;
     })
     .catch(function(err){
-      $location.path('/login');
+      vm.error = 'You must be logged in to see this page';
     });
 });
