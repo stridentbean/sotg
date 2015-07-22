06d596c22b1829bfedd27048c08ab78907dde440
Medhir Bhargava <medhirbhargava@gmail.com>
2015-07-22 11:18:40 -0700

    Buttons highlight on state change

--- client/app/controllers/queryBuilderController.js
+++ client/app/controllers/queryBuilderController.js
@@ -1,13 +1,6 @@
 angular.module('queryBuilderCtrl', [])
 .controller('queryBuilderController', function(Auth, $location, $window, $state) {
   var vm = this;
-  Auth.profile()
-  .then(function(res){
-      vm.username = res.data.username; 
-      vm.apiKey = res.data.apiKey;
-      vm.keywords = res.data.keywords;
-    })
-    .catch(function(err){
-      vm.error = 'You must be logged in to see this page';
-    });
+  vm.$state = $state;
+  console.log(vm.$state.is('queryBuilder.search'));
 });
